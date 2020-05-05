c3ae88c2a16dd92adead43f0484119131575e605
George Goodall <eorgegelliott@hotmail.co.uk>
2020-05-05 17:08:31 +0100

    started about section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React from 'react';
 import backgroundImage from './Assets/Images/1.jpg';
 import './App.css';
 
+import { FaFacebook, FaInstagram, FaYoutube, FaEnvelope } from 'react-icons/fa'
+
 
 function App() {
   return (
     <div className="backgroundContainer">
-        <div className="image"></div>
+        <div className="image">
+          <div className="aboutSection">
+            <h1>Nathan Ellerton</h1>
+            <p>is a bossman when it comes to photos and videos i guess</p>
+            <div className="icons">
+              <FaFacebook className="icon"/>
+              <FaInstagram className="icon"/>
+              <FaYoutube className="icon"/>
+              <FaEnvelope className="icon"/>
+            </div>
+          </div>
+          <div className="navbar">
+          
+          </div>
+        </div>
     </div>
   );
 }
