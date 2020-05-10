51f23e87fe550c88498aa74544761d1f8c73387e
George Goodall <eorgegelliott@hotmail.co.uk>
2020-05-10 19:33:10 +0100

    started changing video style

--- src/Videos/Videos.jsx
+++ src/Videos/Videos.jsx
@@ -8,13 +8,12 @@ const Videos = () => {
     return (
         <div id="Videos" className="Videos">
             <h1 className="VideosTitle">Videos</h1>
-            
-            {videos.map((video)=>{
-                console.log(video)
-                return <VideoComponent videoID={video.id} title={video.title} description={video.description}/>
-            })}
-
-            
+            <div className="VideosContainer">
+                {videos.map((video)=>{
+                    console.log(video)
+                    return <VideoComponent videoID={video.id} title={video.title} description={video.description}/>
+                })}
+            </div>
         </div>
     );
 }
