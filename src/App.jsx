ee021757060f071b6833dbccd762dac3b8b39586
GeorgeGoodall <eorgegelliott@hotmail.co.uk>
2020-05-14 22:02:45 +0100

    setting height variable from js for mobile browsers with colapsable url bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import './App.css';
 import Landing from './Landing'
@@ -8,6 +8,17 @@ import Contact from './Contact/Contact';
 
 
 function App() {
+  
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  useEffect(()=>{
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  })
+  
   return (
     
       <div>
